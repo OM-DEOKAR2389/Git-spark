c762b2cba8bbc00e1edaa21f0936499ae21b71da
omdeokar <omdeokar3106@gmail.com>
2025-07-01 15:19:10 +0530

    change in m method

--- spark.java
+++ spark.java
@@ -1,5 +1,10 @@
 class spark{
     public static void main( String[] args){
         System.out.println("Hello");
+        m();
+    }
+    public static void m()
+    {
+        System.out.println("first method");
     }
 };
